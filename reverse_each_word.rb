0e940b4ff4cc22e020723fee01f26be05792d579
Austin Cummings <aac729@live.com>
2020-08-01 19:22:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   words_array = sentence.split("Hello there, how are you?")
   new_array = ["Hello","there","and","how","are","you"]
   new_array = words_array.collect {|word| word.reverse}
-  
+  new_array.join(" ")
 end 
 reverse_each_word("Hello there, how are you?")
