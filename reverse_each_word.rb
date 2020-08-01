a02527434c2affce0a4620d03c6595059797d7e2
Austin Cummings <aac729@live.com>
2020-08-01 19:19:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
+  words_array = sentence.split("Hello there, how are you?")
+  new_array = ["Hello","there","and","how","are","you"]
   new_array = words_array.collect
 end 
